3197b397cd71b2cd05232c82664e8f6e51cc2f14
VARUN KUMAR <varunjha089@gmail.com>
2021-05-30 20:36:00 +0530

    Added Links to several sites. Changed Background colour of app and Scaffold.

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,17 @@ void main() {
   runApp(
     // Material app is kind of like home screen
     MaterialApp(
-      // Check for Scaffold()
+      //1. https://api.flutter.dev/flutter/material/Scaffold-class.html
       home: Scaffold(
+        //4. https://api.flutter.dev/flutter/material/Scaffold/backgroundColor.html
+        backgroundColor: Colors.blueGrey,
+        //2. https://api.flutter.dev/flutter/material/Scaffold/appBar.html
         appBar: AppBar(
           title: Text("I am Rich"),
+          //3. https://material.io/design/color/the-color-system.html#color-usage-and-palettes
+          backgroundColor: Colors.blueGrey[900],
         ),
+        body: Image(),
       ),
     ),
   );
